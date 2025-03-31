ec24e6639161ff65c0a3ac71c346e12ed5fca85a
shravan kumar <117439152+borrolla@users.noreply.github.com>
2025-03-31 21:11:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const squares = document.querySelectorAll(".square");
 
 // Add event listeners for hover
 squares.forEach((square, index) => {
-    square.addEventListener("mouseenter", () => {
+    square.addEventListener("mouseover", () => {
         squares.forEach((otherSquare, otherIndex) => {
             if (otherIndex !== index) {
                 otherSquare.style.backgroundColor = "#6F4E37"; // Coffee color
@@ -12,7 +12,7 @@ squares.forEach((square, index) => {
         });
     });
 
-    square.addEventListener("mouseleave", () => {
+    square.addEventListener("mouseout", () => {
         squares.forEach((otherSquare) => {
             otherSquare.style.backgroundColor = "#E6E6FA"; // Lavender color
         });
